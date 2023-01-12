5bc4bef9383675d5a8240f9dfc07f7565e2051d5
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-12 15:00:58 +0100

    4

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-1
-var myBody = document.getElementsByTagName("body")[0];
+// 1
+// var myBody = document.getElementsByTagName("body")[0];
+
+// 2
+// console.log(myBody.firstChild);
+
+// 3
+// console.log(myBody.lastChild);
+
+// 4

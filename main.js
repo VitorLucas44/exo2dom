f683f3edf8168ecf3862a36a35353d3571b97bd9
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-12 16:11:32 +0100

    6

--- main.js
+++ main.js
@@ -8,3 +8,16 @@
 // console.log(myBody.lastChild);
 
 // 4
+// let exo4 = document.getElementsByTagName('div')[0].children;
+// console.log(exo4);
+// exo4.forEach(function(child) {
+//     console.log(child);
+// });
+
+// 5
+// var liElements = document.getElementsByTagName("li");
+// console.log(liElements[0].nextSibling);
+
+// 6
+var liElements = document.getElementsByTagName("li");
+console.log(liElements[1].previousSibling);
